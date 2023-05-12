8e89fc1f95e7ddcf99a14f75c14713b362f42ecd
Erik Tapia <20203tn152@utez.edu.mx>
2023-05-12 10:56:58 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine("Ingrese el número de paquetes a empacar:");
+    int paquetes = Convert.ToInt32(Console.ReadLine());
+
+    int cajas = paquetes / 6;
+    int sobrantes = paquetes % 6;
+
+    Console.WriteLine($"Se necesitan {cajas} cajas para empacar los {paquetes} paquetes.");
+    if (sobrantes > 0) {
+      Console.WriteLine($"Sobraron {sobrantes} paquetes sin empacar.");
+    }
+  }
+}
